c79a50a0503d454279b6539b49e5bed05757ae77
DonovanBurrola <donovan.burrola@student.cart.org>
2023-04-25 15:48:23 -0700

    Worked on load page
    almost done, few compatibility issues

---
+++ script.js
@@ -0,0 +1,8 @@
+let handle = document.getElementById('handle')
+let shaft = document.getElementById('shaft')
+
+function leverOpen(){
+    handle.style.bottom = '0'
+    shaft.style.height = '45px'
+    shaft.style.bottom = '0'
+}
