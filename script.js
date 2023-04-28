3a945ed608c8b422e28654776361b1f51f099477
Donovan Burrola <116219378+UnsafeDragoon@users.noreply.github.com>
2023-04-27 21:46:16 -0700

    finish home page
    next time I work i will completely finish html before resuming css and javascript. Scaling for homepage still needs some work, but is not terrible

--- script.js
+++ script.js
@@ -28,9 +28,47 @@ function leverOpen(){
         setTimeout(function(){window.location.href = 'homepage.html'}, 2000 );
      }, 6200 );
 }
-
+window.addEventListener("scroll", function() {
+    var elementTarget = document.querySelector("header");
+    if (window.scrollY === 0) {
+        this.document.querySelector('nav').style.backgroundColor =' rgba(255, 255, 255, 0)'
+        if(location.pathname === '/homepage.html'){
+            this.document.querySelector('.arrow').style.opacity ='1'
+        } else{
+            console.log('none')
+        }
+    } else if (window.scrollY > 0){
+        this.document.querySelector('nav').style.backgroundColor ='rgb(33, 47, 69)'
+        if(location.pathname === '/homepage.html'){
+            this.document.querySelector('.arrow').style.opacity ='0'
+        }
+    }
+  });
 function homePageInit(){
+    document.querySelector('.arrow').style.marginTop='80vh'
+    document.querySelector('nav').style.top ='0'
     document.querySelector('.first').style.right ='-15vw'
     document.querySelector('.last').style.left ='-15vw'
 
 }
+
+
+
+function resizeToMinimum(){
+    var minimum    = [640, 480];
+    var current    = [window.outerWidth, window.outerHeight];
+    var restricted = [];
+    var i          = 2;
+  
+    while(i-- > 0){
+      restricted[i] = minimum[i] > current[i] ? minimum[i] : current[i];
+    }
+  
+    window.resizeTo(current[0], current[1]);
+  }
+  
+  window.addEventListener('resize', resizeToMinimum, false)
+
+
+
+  
